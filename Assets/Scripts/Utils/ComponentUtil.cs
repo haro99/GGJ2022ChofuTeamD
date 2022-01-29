9328c8e7ab64a377efc0a3785bff4d974d87e4f7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-01-29 13:02:09 +0900

    namespace消し忘れ

--- Assets/Scripts/Utils/ComponentUtil.cs
+++ Assets/Scripts/Utils/ComponentUtil.cs
@@ -7,67 +7,64 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 
-namespace VRoidSDK
+public class ComponentUtil
 {
-    public class ComponentUtil
+   /// <summary>
+   /// <para>初期化</para>
+   /// <para>【第1引数】初期化したいTransform</para>
+   /// </summary>
+   public static void Normalize(Transform t)
+   {
+        t.localPosition = Vector3.zero;
+        t.localEulerAngles = Vector3.zero;
+        t.localScale = Vector3.one;
+   }
+
+   /// <summary>
+   /// <para>GameObjectを生成</para>
+   /// <para>【第1引数】親となるGameObject</para>
+   /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
+   /// <para>【戻り値】生成されたGameObject</para>
+   /// </summary>
+   public static GameObject InstantiateTo(GameObject parent, GameObject go)
+   {
+        GameObject ins = (GameObject)GameObject.Instantiate(
+            go,
+            parent.transform.position,
+            parent.transform.rotation
+        );
+        ins.transform.parent = parent.transform;
+        Normalize(ins.transform);
+        return ins;
+   }
+
+    /// <summary>
+    /// <para>GameObjectを生成し、そのGameObjectに張り付いている、Componentクラスを取得する</para>
+    /// <para>【第1引数】親となるGameObject</para>
+    /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
+    /// <para>【戻り値】生成されたGameObjectに張り付いてる指定したComponentクラス</para>
+    /// </summary>
+    public static T InstantiateTo<T>(GameObject parent, GameObject go)
+        where T : Component
     {
-        /// <summary>
-        /// <para>初期化</para>
-        /// <para>【第1引数】初期化したいTransform</para>
-        /// </summary>
-        public static void Normalize(Transform t)
-        {
-            t.localPosition = Vector3.zero;
-            t.localEulerAngles = Vector3.zero;
-            t.localScale = Vector3.one;
-        }
+        return InstantiateTo(parent, go).GetComponent<T>();
+    }
 
-        /// <summary>
-        /// <para>GameObjectを生成</para>
-        /// <para>【第1引数】親となるGameObject</para>
-        /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
-        /// <para>【戻り値】生成されたGameObject</para>
-        /// </summary>
-        public static GameObject InstantiateTo(GameObject parent, GameObject go)
+    /// <summary>
+    /// <para>Transform以下に紐付いている子のGameObjectを全て削除する</para>
+    /// <para>【第1引数】削除したいTransform</para>
+    /// </summary>
+    public static void DeleteAllChildren(Transform parent)
+    {
+        List<Transform> transformList = new List<Transform>();
+        foreach (Transform child in parent)
         {
-            GameObject ins = (GameObject)GameObject.Instantiate(
-                go,
-                parent.transform.position,
-                parent.transform.rotation
-            );
-            ins.transform.parent = parent.transform;
-            Normalize(ins.transform);
-            return ins;
+            transformList.Add(child);
         }
-
-        /// <summary>
-        /// <para>GameObjectを生成し、そのGameObjectに張り付いている、Componentクラスを取得する</para>
-        /// <para>【第1引数】親となるGameObject</para>
-        /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
-        /// <para>【戻り値】生成されたGameObjectに張り付いてる指定したComponentクラス</para>
-        /// </summary>
-        public static T InstantiateTo<T>(GameObject parent, GameObject go)
-            where T : Component
+        parent.DetachChildren();
+        foreach (Transform child in transformList)
         {
-            return InstantiateTo(parent, go).GetComponent<T>();
-        }
-
-        /// <summary>
-        /// <para>Transform以下に紐付いている子のGameObjectを全て削除する</para>
-        /// <para>【第1引数】削除したいTransform</para>
-        /// </summary>
-        public static void DeleteAllChildren(Transform parent)
-        {
-            List<Transform> transformList = new List<Transform>();
-            foreach (Transform child in parent)
-            {
-                transformList.Add(child);
-            }
-            parent.DetachChildren();
-            foreach (Transform child in transformList)
-            {
-                GameObject.Destroy(child.gameObject);
-            }
+            GameObject.Destroy(child.gameObject);
         }
     }
 }
