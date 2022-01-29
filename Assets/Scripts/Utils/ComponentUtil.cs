5d02b5524753605559d797b318d7bfe38bf2919c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-01-29 11:59:46 +0900

    まずはSingletonの母体を持ってきてGameControllerを作った

---
+++ Assets/Scripts/Utils/ComponentUtil.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace VRoidSDK
+{
+    public class ComponentUtil
+    {
+        /// <summary>
+        /// <para>初期化</para>
+        /// <para>【第1引数】初期化したいTransform</para>
+        /// </summary>
+        public static void Normalize(Transform t)
+        {
+            t.localPosition = Vector3.zero;
+            t.localEulerAngles = Vector3.zero;
+            t.localScale = Vector3.one;
+        }
+
+        /// <summary>
+        /// <para>GameObjectを生成</para>
+        /// <para>【第1引数】親となるGameObject</para>
+        /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
+        /// <para>【戻り値】生成されたGameObject</para>
+        /// </summary>
+        public static GameObject InstantiateTo(GameObject parent, GameObject go)
+        {
+            GameObject ins = (GameObject)GameObject.Instantiate(
+                go,
+                parent.transform.position,
+                parent.transform.rotation
+            );
+            ins.transform.parent = parent.transform;
+            Normalize(ins.transform);
+            return ins;
+        }
+
+        /// <summary>
+        /// <para>GameObjectを生成し、そのGameObjectに張り付いている、Componentクラスを取得する</para>
+        /// <para>【第1引数】親となるGameObject</para>
+        /// <para>【第2引数】生成したいGameObject(外部から持ってきた物)</para>
+        /// <para>【戻り値】生成されたGameObjectに張り付いてる指定したComponentクラス</para>
+        /// </summary>
+        public static T InstantiateTo<T>(GameObject parent, GameObject go)
+            where T : Component
+        {
+            return InstantiateTo(parent, go).GetComponent<T>();
+        }
+
+        /// <summary>
+        /// <para>Transform以下に紐付いている子のGameObjectを全て削除する</para>
+        /// <para>【第1引数】削除したいTransform</para>
+        /// </summary>
+        public static void DeleteAllChildren(Transform parent)
+        {
+            List<Transform> transformList = new List<Transform>();
+            foreach (Transform child in parent)
+            {
+                transformList.Add(child);
+            }
+            parent.DetachChildren();
+            foreach (Transform child in transformList)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/SingletonBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SingletonBehaviour<T> : MonoBehaviour
+	where T : MonoBehaviour
+{
+	private static T instance;
+
+	[SerializeField]
+	bool dontDestroyOnLoad;
+
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = (T)FindObjectOfType(typeof(T));
+				if (instance == null) throw new System.Exception("Error: " + typeof(T) + " is nothing.");
+			}
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		CheckInstance ();
+	}
+
+	void Start()
+	{
+		SingleStart ();
+	}
+
+	protected bool CheckInstance()
+	{
+		if(this == Instance)
+		{ 
+			if(dontDestroyOnLoad) DontDestroyOnLoad(this.gameObject);
+			SingleAwake();
+			return true;
+		}
+		Destroy(this.gameObject);
+		return false;
+	}
+
+	public virtual void SingleAwake() {}
+	public virtual void SingleStart() {}
+
+	public static bool Exist 
+	{
+		get
+		{
+			return instance != null;
+		}
+	}
+}
