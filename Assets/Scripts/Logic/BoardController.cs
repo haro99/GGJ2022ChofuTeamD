fe4456cc2083bece0b175f272e2befbde0d41d32
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-01-29 12:24:17 +0900

    まずは盤面の土台を作った

---
+++ Assets/Scripts/Logic/BoardController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    // 盤面の横幅のマス数(とりあえず8マス)
+    [SerializeField] private int boardWidth;
+    // 盤面の縦幅のマス数(とりあえず10マス)
+    [SerializeField] private int boardHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
